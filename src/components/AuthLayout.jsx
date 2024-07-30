c860e6f1744b345205e4a8a5b61411cf87624c7f
rishabh8911 <rsbh1125@gmail.com>
2024-07-30 08:18:49 +0530

    add , login, auth,signup

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,34 @@
+import React , {useEffect,useState} from 'react'
+import  {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import authSlice from '../store/authSlice'
+
+
+
+export default function Protected ({children , authentication=true}){
+    const navigate = useNavigate()
+    const [loader,setLoader]= useState(true)
+    const authStatus = useSelector(state=> state=>state.auth.status
+        
+    )
+
+    useEffect(() => {
+
+
+        if (authentication && authStatus !== authentication){
+            navigate ("/login")
+        } else if (!authentication && authStatus !==
+            authentication ){
+            navigate("/")
+        }
+        setLoader(false)
+
+    }, [authStatus, navigate, authentication])
+
+
+    return loader ?  <h1> Loading...</h1> : <>{children}</>
+    
+
+
+
+}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
         try {
             const session = await authService.login(data)
             if(session){
-                const useData = await authService.
+                const userData = await authService.
                 getCurrentUser()
                 if(userData) dispatch(authLogin(userData))
                     navigate("/")
@@ -43,6 +43,49 @@ function Login() {
                             <Logo width="100%" />
                         </span>
                     </div>
+                    <h2 className='text-center text-2xl front-bold
+                    leading-tonight'>Sign in to your account</h2>
+                    <p className='mt-2 text-center text-base text-black/60'>
+                    Don&apos;t have any account?&nbsp;
+                    <Link
+                    to="/signup"
+                    className='front-medium text-primary
+                    transition-all duration-200 hover:underline'
+                    
+                    >
+                        Sign Up
+                    </Link>
+                    </p>
+                    {error && <p className='text-red-600 mt-8
+                    text-center'>{error}</p>}
+                    <form onSubmit={handleSubmit(login)}
+                    className='mt-8'
+                    >
+                        <div className='space-y-5'>
+                            <Input 
+                            label= "Email: "
+                            placeholder ="Enter your email"
+                            type ="email"
+                            {...register("email",{
+                                required: true,
+                                validate: {
+                                    matchPatern :   ||
+                                    "Email address must be a valid address",
+
+
+                                     
+                                }
+                            })}
+                            />
+                            <Button
+                            type='submit'
+                            className='w-full'>
+                                Sign in
+                            </Button>
+
+                        </div>
+                    </form>
+                    
                 </div>
         </div>
     )
